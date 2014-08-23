bcec2c54063b8e0020a80a4e77aa45903e23d46f
Nikita Koval <parusnikita@gmail.com>
2014-08-24 01:39:30 +0400

    Dummy allocator for STL containers #1

--- test/mmap_allocator_test.cpp
+++ test/mmap_allocator_test.cpp
@@ -1,20 +1,23 @@
 #include <vector>
 
 #include "gtest/gtest.h"
-#include "mmap_allocator.h"
+#include "mmap_std_allocator.h"
+//#include "dummy_allocator.h"
 
 TEST(mmap_allocator, alloc)
 {
-    hoard::mmap_allocator<int> a;
-    std::vector<int> v(a);
+    std::vector<int, hoard::mmap_allocator<int>> v;
     for (int i = 0; i < 1000; i++) {
         v.push_back(i);
+    }
+
+    for (int i = 0; i < 1000; i++) {
+        ASSERT_EQ(v[i], i);
     }
 }
 
 TEST(mmap_allocator, free)
 {
-    hoard::mmap_allocator<int> a;
-    std::vector<int> v(a);
+    std::vector<int, hoard::mmap_allocator<int>> v;
     v.~vector();
 }
