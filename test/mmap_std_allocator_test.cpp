161846f97b1d2bdd77d581bf5f4705ae5507879b
Nikita Koval <parusnikita@gmail.com>
2014-08-24 22:35:44 +0400

    mmap_std_allocator does not support complex structures #12

--- test/mmap_std_allocator_test.cpp
+++ test/mmap_std_allocator_test.cpp
@@ -3,10 +3,9 @@
 #include "gtest/gtest.h"
 #include "mmap_std_allocator.h"
 
-TEST(mmap_allocator, alloc)
+TEST(mmap_std_allocator, alloc)
 {
-    malloc(100000);
-    std::vector<int> v;
+    std::vector<int, hoard::mmap_std_allocator<int>> v;
     for (int i = 0; i < 1000; i++) {
         v.push_back(i);
     }
@@ -16,8 +15,22 @@ TEST(mmap_allocator, alloc)
     }
 }
 
-TEST(mmap_allocator, free)
+TEST(mmap_std_allocator, free)
 {
     std::vector<int, hoard::mmap_std_allocator<int>> v;
     v.~vector();
+}
+
+TEST(mmap_std_allocator, complex_structures)
+{
+    std::vector<std::vector<int>, hoard::mmap_std_allocator<std::vector<int> > > v;
+
+    for (int i = 0; i < 1000; i++) {
+        v.push_back(std::vector<int>());
+        v[i].push_back(i);
+    }
+
+    for (int i = 0; i < 1000; i++) {
+        ASSERT_EQ(v[i][0], i);
+    }
 }
