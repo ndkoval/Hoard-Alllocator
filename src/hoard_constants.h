a43c20a0a98e0a2fe70411ef8dfb0290fa65789d
Nikita Koval <parusnikita@gmail.com>
2014-08-26 01:09:56 +0400

    Change internals #7

--- src/hoard_constants.h
+++ src/hoard_constants.h
@@ -5,14 +5,13 @@ namespace hoard {
     constexpr size_t BLOCK_FACTOR = 2; // power of 2  and > 1
     constexpr size_t MIN_BLOCK_SIZE = 8;  //power of BLOCK_FACTOR
     constexpr size_t EMPTYNESS_FACTOR = 4; // power of 2; 1/EMPTYNESS_FACTOR = f from hoard doc
-    size_t PAGE_SIZE =  sysconf(_SC_PAGESIZE);
+    const size_t PAGE_SIZE = sysconf(_SC_PAGESIZE);
     constexpr size_t SUPERBLOCK_SIZE = 4 * 1024 * 8; // power of 2
-    size_t PAGES_IN_SUPERBLOCK = SUPERBLOCK_SIZE / PAGE_SIZE; // power of 2
-    size_t NUMBER_OF_CPU = sysconf( _SC_NPROCESSORS_ONLN );
-    size_t NUMBER_OF_HEAPS = NUMBER_OF_CPU * 2;
+    const size_t PAGES_IN_SUPERBLOCK = SUPERBLOCK_SIZE / PAGE_SIZE; // power of 2
+    const size_t NUMBER_OF_CPU = sysconf( _SC_NPROCESSORS_ONLN );
+    const size_t NUMBER_OF_HEAPS = NUMBER_OF_CPU * 2;
     constexpr int MAGIC_NUMBER = 0xdeadbeef;
     //static size_t
-
 
 }
 
