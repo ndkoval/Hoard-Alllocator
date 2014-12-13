0a828810498e29ed8e813a6362fc6a2743544520
Vladimir Skipor <vladimirskipor@gmail.com>
2014-12-13 21:21:21 +0300

    testing helping macros were changed

--- src/test/noalloc_testing_test.cpp
+++ src/test/noalloc_testing_test.cpp
@@ -2,12 +2,11 @@
 #include "noalloc_testing.h"
 
 int main() {
-    auto * ptr =  malloc(16);
+    auto ptr = nullptr;
     EXPECT_TRUE(ptr == nullptr);
     EXPECT_FALSE(ptr);
     ASSERT_TRUE(ptr == nullptr);
     ASSERT_FALSE(ptr);
-    free(ptr);
 
     return TestResult();
 }
