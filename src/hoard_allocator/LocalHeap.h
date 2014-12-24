5e3093932513ec98a0907e52b77ab3d332f6f9b5
Vladimir Skipor <vladimirskipor@gmail.com>
2014-12-24 17:46:23 +0300

    LocalHeap Alloc

--- src/hoard_allocator/LocalHeap.h
+++ src/hoard_allocator/LocalHeap.h
@@ -10,13 +10,33 @@ namespace hoard {
 class LocalHeap : public BaseHeap {
 
 constexpr static size_t kBinCount = 7; //blocks_allocated / size = 0,  <=1/8, <=1/4, <=2/4, <=3/4, <1, 1
+constexpr static size_t kAlmostFullBlocksBinNum = 6; // 3/4<  blocks_allocated / size <1
+constexpr static size_t kEmptySuperblocksBinNum = 0; // blocks_allocated / size = 0
+
 public:
 	LocalHeap(GlobalHeap *parent_heap) : parent_heap_(parent_heap), blocks_allocated_(0), blocks_size_(0) {
 	}
 
 	void * Alloc() {
+		lock_guard guard(lock);
+		if (blocks_allocated_ == blocks_size_) {
+			GetSuperblock();
+		}
 
-		return nullptr;
+		for(int i_bin = kAlmostFullBlocksBinNum; i_bin >= 0; --i_bin) {
+			SuperblockStack & bin = bins_[i_bin];
+			if (!bin.IsEmpty()) {
+				SuperblockHeader & header = bin.Top()->header();
+				void * allocated =  header.Alloc();
+				const int new_bin_num = BinNum(header.blocks_allocated(), header.block_size());
+				if (new_bin_num != i_bin) {
+					Superblock *superblock = bin.Pop();
+					bins_[new_bin_num].Push(superblock);
+				}
+				return allocated;
+			}
+		}
+		assert(false && "no superblock choosed, shouldn't happen");
 	}
 
 	virtual void OnFreeSuperblock(Superblock *superblock) override {
@@ -32,6 +52,14 @@ private:
 	std::array<SuperblockStack, kBinCount> bins_;
 	size_t blocks_allocated_;
 	size_t blocks_size_;
+
+	void GetSuperblock() {
+
+	}
+
+	void TransferSuperblock() {
+
+	}
 
 	size_t BinNum(size_t blocs_allocated, size_t blocs_size) {
 		switch (RoundUp(blocs_allocated * 8, blocs_size)) {
