19be8b9140265bbb938b33d7c877549d58d8d432
Vladimir Skipor <vladimirskipor@gmail.com>
2014-12-24 14:26:37 +0300

    merge problem fix

--- src/hoard_allocator/dummy/dummy_malloc_intercept.cpp
+++ src/hoard_allocator/dummy/dummy_malloc_intercept.cpp
@@ -1,40 +1,43 @@
+#include <hoard_constants.h>
 #include "tracing.h"
 
 extern "C"
-void *malloc(size_t size) {
+void *malloc(size_t size) __THROW {
   hoard::trace("malloc: ", size);
-  abort();
+  return nullptr;
 }
 
 extern "C"
-void *calloc(size_t n, size_t size) {
+void *calloc(size_t n, size_t size) __THROW {
   hoard::trace("calloc: n=", n, "; size=", size);
-  abort();
+  return nullptr;
 }
 
 extern "C"
-void free(void *ptr) {
+void free(void *ptr) __THROW {
   hoard::trace("free: ", ptr);
-  abort();
 }
 
 extern "C"
-void *realloc(void *ptr, size_t size) {
+void *realloc(void *ptr, size_t size) __THROW {
   hoard::trace("realloc: ptr=", ptr, "; size=", size);
-  abort();
+  return nullptr;
 }
 
 extern "C"
-int posix_memalign(void **memptr, size_t alignment, size_t size) {
-  abort();
+int posix_memalign(void **memptr, size_t alignment, size_t size) __THROW {
+  hoard::trace("posix_memalign");
+  std::abort();
 }
 
 extern "C"
-void *valloc(size_t size) {
-  abort();
+void *valloc(size_t size) __THROW {
+  hoard::trace("valloc: ", size);
+  return nullptr;
 }
 
 extern "C"
-void *memalign(size_t boundary, size_t size) {
-  abort();
+void *memalign(size_t boundary, size_t size) __THROW {
+  hoard::trace("memalign: ", boundary, ' ', size);
+  return nullptr;
 }
