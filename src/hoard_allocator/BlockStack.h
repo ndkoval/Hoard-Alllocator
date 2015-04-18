f894513279bc4e3465389bb165a96a82958093eb
Vladimir Skipor <vladimirskipor@gmail.com>
2015-04-18 23:02:05 +0300

    remove virtual functions call for stack

--- src/hoard_allocator/BlockStack.h
+++ src/hoard_allocator/BlockStack.h
@@ -2,25 +2,60 @@
 #define BLOCK_STACK_H
 
 #include "Block.h"
-#include "BaseStack.h"
 
 namespace hoard {
 
-class BlockStack : public BaseStack<Block> {
+  class BlockStack  {
 
-protected:
-	virtual void SetNext(Block *block, Block *value) override {
-		block->set_next(value);
-	}
 
-	virtual void SetPrev(Block *block, Block *value) override {
-		// not needed
-	}
+  public:
+    BlockStack() : head_(nullptr) {
+    }
 
-	virtual Block *Next(Block *block) override {
-		return block->next();
-	}
-};
+    BlockStack(const BlockStack &) = delete;
+    BlockStack & operator=(const BlockStack &) = delete;
+
+    void Push(Block *t) {
+      t->set_next(head_);
+      head_ = t;
+    }
+
+    Block *Pop() {
+      assert(!IsEmpty());
+      Block * const t = head_;
+      head_ = head_->next();
+//    t->set_next(nullptr);
+      return t;
+    }
+
+    Block * Top() {
+      return head_;
+    }
+
+    bool IsEmpty() {
+      return head_ == nullptr;
+    }
+
+    void Reset() {
+      head_ = nullptr;
+    }
+
+    bool Contains(Block * element) {
+      auto * current = head_;
+      while (current != nullptr) {
+        if (element == current) {
+          return true;
+        }
+        current = current->next();
+      }
+      return false;
+    }
+
+
+  protected:
+    Block *head_;
+
+  };
 
 }
 #endif // BLOCK_STACK_H
